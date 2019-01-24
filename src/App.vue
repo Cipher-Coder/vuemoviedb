9239bc3c4401e25199437d8522b2678274160e05
Cipher-Coder <matthew.rebehn@gmail.com>
2019-01-23 22:36:19 -0500

    progressing with moviedb #10

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="app">
+    <Header :title="title" />
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ hello }}</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
+
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      hello: 'Hello World',
+      title: 'Vue Movie DB'
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
+
+
 
 <style>
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<style scoped>
+
+header {
+    background-color: #111;
+    padding: 20px;
+    color: white;
+}
+
+h1 {
+    margin: 0;
+}
+
+</style>
